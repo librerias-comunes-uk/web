b0949a0f1329add655dc49d96dca441039433a00
Miguel Angel Gil <miguelangel.gil@samelan.com>
2020-10-16 12:53:44 +0200

    web

---
+++ src/main/java/org/springplugins/web/SelectedAnnotationHandlerMapping.java
@@ -0,0 +1,33 @@
+package org.springplugins.web;
+
+import java.util.ArrayList;
+import org.springframework.web.servlet.mvc.annotation.DefaultAnnotationHandlerMapping;
+
+public class SelectedAnnotationHandlerMapping extends DefaultAnnotationHandlerMapping {
+  private ArrayList a;
+  
+  public void setUrls(ArrayList paramArrayList) {
+    this.a = paramArrayList;
+  }
+  
+  public String[] getFiltered(String[] paramArrayOfString) {
+    if (paramArrayOfString == null)
+      return null; 
+    ArrayList<String> arrayList = new ArrayList();
+    for (String str : paramArrayOfString) {
+      if (this.a.contains(str))
+        arrayList.add(str); 
+    } 
+    return arrayList.<String>toArray(new String[arrayList.size()]);
+  }
+  
+  protected String[] determineUrlsForHandler(String paramString) {
+    return getFiltered(super.determineUrlsForHandler(paramString));
+  }
+}
+
+
+/* Location:              C:\Users\MiguelAngel\Downloads\web-1.0.0.jar!\org\springplugins\web\SelectedAnnotationHandlerMapping.class
+ * Java compiler version: 5 (49.0)
+ * JD-Core Version:       1.1.3
+ */
